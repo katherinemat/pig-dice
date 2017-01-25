23c5c242b4ee25999e8b952ff4bb2f0e590789de
Kaz Matthews and Tammy Dang <kaz@>
2017-01-25 09:28:08 -0800

    define rule for current score to get reset to zero when player rolls a 1

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,15 @@
 
 //User Interface
 $(document).ready(function() {
+  var currentRoll = 0;
   $(".roll").click(function() {
     var rollDice = Math.floor(Math.random()*6)+1;
     $("#p1-dice-roll").empty().append(rollDice);
+    currentRoll += rollDice;
+    if(rollDice === 1) {
+      currentRoll = 0;
+    }
+    $("#p1-current-roll").empty().append(currentRoll);
   });
+
 })
