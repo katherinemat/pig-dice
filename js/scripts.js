56e4ad9bcb84dcc1c4c724bcbb198f42f21d63c5
Kaz Matthews and Tammy Dang <tammy@>
2017-01-25 13:19:23 -0800

    duplicate functions and event listeners from player 1 into player 2. also expand object to include score and overall score for player 2.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,9 @@
 //Business Logic
 var diceGame = {
-  currentRoll: 0,
+  p1CurrentRoll: 0,
   p1Score: 0,
+  p2CurrentRoll: 0,
+  p2Score: 0,
 };
 
 var rollDice = function() {
@@ -10,22 +12,40 @@ var rollDice = function() {
 
 //User Interface
 $(document).ready(function() {
-  $(".roll").click(function() {
+  $(".roll1").click(function() {
     var roll = rollDice();
     $("#p1-dice-roll").empty().append(roll);
-    diceGame.currentRoll += roll;
+    diceGame.p1CurrentRoll += roll;
     if(roll === 1) {
-      diceGame.currentRoll = 0;
+      diceGame.p1CurrentRoll = 0;
     }
-    $("#p1-current-roll").empty().append(diceGame.currentRoll);
+    $("#p1-current-roll").empty().append(diceGame.p1CurrentRoll);
   });
-  $(".hold").click(function() {
-    diceGame.p1Score += diceGame.currentRoll;
+  $(".hold1").click(function() {
+    diceGame.p1Score += diceGame.p1CurrentRoll;
     $("#p1-score").empty().append(diceGame.p1Score);
-    diceGame.currentRoll = 0;
-    if(diceGame.p1Score >= 10) {
+    diceGame.p1CurrentRoll = 0;
+    if(diceGame.p1Score >= 100) {
       $("#results").show();
-      $(".winner").append("player 1");
+      $(".winner").append("PLAYER 1");
+    }
+  });
+  $(".roll2").click(function() {
+    var roll = rollDice();
+    $("#p2-dice-roll").empty().append(roll);
+    diceGame.p2CurrentRoll += roll;
+    if(roll === 1) {
+      diceGame.p2CurrentRoll = 0;
+    }
+    $("#p2-current-roll").empty().append(diceGame.p2CurrentRoll);
+  });
+  $(".hold2").click(function() {
+    diceGame.p2Score += diceGame.p2CurrentRoll;
+    $("#p2-score").empty().append(diceGame.p2Score);
+    diceGame.p2CurrentRoll = 0;
+    if(diceGame.p2Score >= 100) {
+      $("#results").show();
+      $(".winner").append("PLAYER 2");
     }
   });
 });
